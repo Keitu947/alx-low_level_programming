d615558e35f9353838817e6b9ba6e1497d072586
keitumetse mokwena <keitumetsetwin@gmail.com>
2022-06-30 13:49:30 -0700

    Hello print

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 
-        puts("\"Programming is like building a multilingual puzzle");
+       printf("with proper grammar, but the outcome is a piece of art,\n");
 
-		return (0);
+       return (0);
 
 }
