22ff9c142524421599fcf469e4516b4958b6f673
keitumetse mokwena <keitumetsetwin@gmail.com>
2022-06-30 20:08:36 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,12 @@
  */
 int main(void)
 {
+	int i;
+	long int l;
+	long long int ll;
+	float d;
+	char c;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
