25ec66f0587c95270d160ce69dee9ed39e9533d6
keitumetse mokwena <keitumetsetwin@gmail.com>
2022-07-05 18:50:27 -0700

    Print aphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,13 @@
 * print_alphabet_x10 - prints alphabet in lower case x 10.
 *
 * Return: Always 0 (Success)
-* */
-
+*/
 void print_alphabet_x10(void)
 {
 	int x = 0;
 
 	for  (x = 0; x < 10; x++)
-       	{
+	{
 		char i;
 
 		for (i = 'a'; i <= 'z'; i++)
