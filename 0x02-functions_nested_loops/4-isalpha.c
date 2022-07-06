f670a85fab40619eb06d6e4d7e6cbda0c9fa716f
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2022-07-06 04:23:45 +0200

    Add isalpha function

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lower or upper case letter
+ * @c : character to check the case
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+}
