41f78c34594eadf0156e2980f9c6ac120cce1166
keitumetse mokwena <keitumetsetwin@gmail.com>
2022-07-05 18:06:28 -0700

    Update alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,17 @@
 #include "main.h"
-#include "_putchar.c"
+
 /**
- * print_alphabet - prints alphabet in lower case.
- *
- * Return: Always 0 (Success)
- */
+* print_alphabet - prints alphabet in lower case.
+*
+* Return: Always 0 (Success)
+*/
 void print_alphabet(void)
 {
 	int i;
 
 	for (i = 97; i <= 122; i++)
 	{
-		_putchar(i+'');
-		_putchar('\n');
+		_putchar(i);
 	}
+	_putchar('\n');
 }	
