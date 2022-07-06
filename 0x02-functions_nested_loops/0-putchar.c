cf3edb4328b7cb341dafc859d041ae32126eefd9
keitumetse mokwena <keitumetsetwin@gmail.com>
2022-07-05 17:59:16 -0700

    Remove whitespace

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - main block
- * Description: print _putchar followed on a new line 
- * Return: always 0
- */
+* main - main block
+* Description: print _putchar followed on a new line
+* Return: always 0
+*/
 int main(void)
 {
 	_putchar('_');
