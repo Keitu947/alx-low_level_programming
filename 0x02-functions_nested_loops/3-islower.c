bbece914287c3cc4c1fe8d5a72d32ef96da84e7d
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2022-07-06 04:10:19 +0200

    Create _islower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+int _islower(int c)
+{
+	if (islower(c))
+  {
+    return 0;
+  }
+  
+  return 1;
+}
