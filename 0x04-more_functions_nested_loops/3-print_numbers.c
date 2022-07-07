436462218c419444cbf5c06a69dda6cfb2d52cf0
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2022-07-08 00:43:33 +0200

    update print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* print_numbers - prints numbers, from 0 to 9, followed by a new line
+* print_numbers - prints numbers 
 * Return: 0
 */
 void print_numbers(void)
 {
 	char n = 0;
-
+	
 	while (n <= 9)
 	{
 		_putchar('0' + n);
