e8be4f159b348bdeb3bce5a8b5a74398184f87c5
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2022-07-11 01:03:14 +0200

    fix print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,17 +8,20 @@ int _putchar(char c);
 /**
 * print_number - prints numbers
 * @n: number to be printed
-* Return: void
+* Return:void
 */
+
 void print_number(int n)
 {
+
 	unsigned int n1 = 0;
 
-	if (n < 0)
+	if  (n < 0)
 	{
 		n1 = -n;
 		_putchar('-');
 	}
+
 	else
 	{
 		n1 = n;
@@ -27,5 +31,6 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
+
 	_putchar((n1 % 10) + '0');
 }
