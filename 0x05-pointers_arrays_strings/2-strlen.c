c95f7b9b05ff0a48f016caa234124721eefd137e
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2022-07-12 00:20:12 +0200

    Add _strlen function and logic

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+* @s:  char pointer
+* Return: length of a string
+*/
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s[size] != NULL)
+	{
+		size++;
+	}
+
+	return (size);
+}
