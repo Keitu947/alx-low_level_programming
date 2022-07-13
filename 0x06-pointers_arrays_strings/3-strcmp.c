6147b43b26bc870015a79ed437afe9bf73d5978f
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2022-07-13 21:42:32 +0200

    add 3-strcmp.c functions

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strcmp - Compares two strings.
+* @s1: A pointer to the first string to be compared.
+* @s2: A pointer to the second string to be compared.
+*
+* Return: If str1 < str2, the negative difference
+*         If str1 == str2, 0.
+*         If str1 > str2, the positive difference
+*/
+int strcmp (const char* str1, const char* str2);
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
