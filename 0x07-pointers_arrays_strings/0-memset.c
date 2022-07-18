7d23e10c56a1d4a49315ab4992d579dfa122dcf2
keitumetse mokwena <106819479+Keitu947@users.noreply.github.com>
2022-07-18 22:13:04 +0200

    update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-* _memset - fills memory with a constant byte.
+* _memset - Fills the first n bytes of the memory area
 * pointed to by @s with the constant byte @c.
+* @s: A pointer to the memory area to be filled.
 * @b: The character to fill the memory area with.
 * @n: The number of bytes to be filled.
 *
